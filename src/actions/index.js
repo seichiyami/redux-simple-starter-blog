c2927b57bc5cf38e4800125fc9e75c59d837dec3
Brandon Huebert <brandonhuebert@Brandons-MacBook-Pro.local>
2017-07-05 02:22:50 -0700

    Add posts new component, and posts show component.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,32 @@ import axios from 'axios';
 import { POSTS_API_KEY } from '../../config';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${POSTS_API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${POSTS_API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${POSTS_API_KEY}`)
 
   return {
     type: FETCH_POSTS,
