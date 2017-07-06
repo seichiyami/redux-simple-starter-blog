dff142466d704cc044e1609986fd39a0a1a5fcde
Brandon Huebert <brandonhuebert@Brandons-MacBook-Pro.local>
2017-07-05 23:06:05 -0700

    Add delete functionality. Add logic to call fetch post once.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
-    const  { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    // if post already exists in state don't fetch again. May lead to stale data.
+    if (!this.props.post) {
+      const  { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -18,7 +29,13 @@ class PostsShow extends Component {
 
     return (
       <div>
-        <Link to="/">Back to Index</Link>
+        <Link to="/">&lt; Back to posts.</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -29,8 +46,7 @@ class PostsShow extends Component {
 
 function mapStateToProps({ posts }, ownProps) {
   const post = { post: posts[ownProps.match.params.id] };
-  console.log(post);
   return post;
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
